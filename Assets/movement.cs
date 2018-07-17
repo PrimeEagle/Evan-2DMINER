335664518a93f96108869161b670939ec1db40dc
Evan Varan <evan.varan@gmail.com>
2018-07-17 22:11:18 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/Unity/Evan/2DMINER@4 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ Assets/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+    public float moveSpeed = 3f;
+    
+    // Update is called once per frame
+    void Update () {
+        
+        
+        if (Input.GetKey(KeyCode.D))
+        {    
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+    }
+}
